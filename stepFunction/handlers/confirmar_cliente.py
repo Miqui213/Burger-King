2c7d12deebd3ac3880ccf9080a6eada8fe95e5a3
Hector Espinoza <hector.espinoza@utec.edu.pe>
2026-07-08 20:21:14 -0500

    Arreglando bug

--- stepFunction/handlers/confirmar_cliente.py
+++ stepFunction/handlers/confirmar_cliente.py
@@ -1,39 +1,45 @@
 import json
-import os
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
 sfn_client = boto3.client('stepfunctions')
-TABLA_PEDIDOS = os.environ.get('TABLA_PEDIDOS')
+
+TABLA = 'Burger-Pedidos-dev'
 
 def lambda_handler(event, context):
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": True
+    }
+    
     try:
         body = json.loads(event.get('body', '{}'))
         pedido_id = body.get('pedido_id')
 
         if not pedido_id:
-            return {"statusCode": 400, "body": json.dumps({"error": "Falta pedido_id"})}
+            return {"statusCode": 400, "headers": headers, "body": json.dumps({"error": "Falta pedido_id"})}
 
-        table = dynamodb.Table(TABLA_PEDIDOS)
+        # Aquí es donde ocurría el error porque TABLA estaba vacío
+        table = dynamodb.Table(TABLA)
         
-        # Rescatamos el ÚLTIMO token (el que dejó entregado.py)
+        # 2. Rescatamos el Token
         response = table.get_item(Key={'pedido_id': pedido_id})
         item = response.get('Item', {})
         task_token = item.get('taskToken')
 
         if not task_token:
-            return {"statusCode": 400, "body": json.dumps({"error": "No hay token activo para este pedido"})}
+            return {"statusCode": 400, "headers": headers, "body": json.dumps({"error": "No hay token activo para este pedido."})}
 
-        # Armamos el Payload final
+        # 3. Armamos la 'Muñeca Rusa' final
         sfn_output = {
-            "status": "CLIENTE_CONFIRMA",
+            "status": "ENTREGADO", 
             "input": {
                 "pedido_id": pedido_id,
-                "mensaje": "El cliente tiene la hamburguesa en sus manos"
+                "mensaje": "El cliente confirmó la recepción"
             }
         }
 
-        # Despertamos a la Step Function por última vez
+        # 4. Despertamos la Step Function
         sfn_client.send_task_success(
             taskToken=task_token,
             output=json.dumps(sfn_output)
@@ -41,9 +47,11 @@ def lambda_handler(event, context):
 
         return {
             "statusCode": 200,
+            "headers": headers,
             "body": json.dumps({"message": "¡Gracias por confirmar! Buen provecho."})
         }
 
     except Exception as e:
         print(f"Error: {str(e)}")
-        return {"statusCode": 500, "body": json.dumps({"error": "Error interno del servidor"})}
+        # Ahora el error exacto se enviará al navegador
+        return {"statusCode": 500, "headers": headers, "body": json.dumps({"error": f"Fallo interno: {str(e)}"})}
